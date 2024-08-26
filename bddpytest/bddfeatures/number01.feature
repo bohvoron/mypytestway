43b8ced8b8a0d874ba865ff8bf28f20e1fd7b362
bohvoron <160781963+bohvoron@users.noreply.github.com>
2024-08-26 18:18:46 +0200

    fixed a bug

--- bddpytest/bddfeatures/number01.feature
+++ bddpytest/bddfeatures/number01.feature
@@ -2,10 +2,10 @@ Feature: Transaction Bank Acc
     Tests pertaining to banks Transactions (like withdrawal, deposit and etc)
     
 
-    Scenario: 
-        Given The balance is 1000  
-        When the acc holder withdraws 300 
-        Then balans should to be 700  
+    Scenario: Withdrawal of money
+        Given The balance is 1000
+        When the acc holder withdraws 300
+        Then balans should to be 700
  
 
 
--- bddpytest/test_bdd01.py
+++ bddpytest/test_bdd01.py
@@ -1,3 +1,4 @@
+import pytest 
 from pytest_bdd import scenario, scenarios, given, when, then
 from pathlib import Path
 
@@ -15,14 +16,14 @@ def test_withdrawal():
     print('End of test Withdrawal')
     pass
 
-@given('The balance is 1000 ')
+@given('The balance is 1000')
 def current_balance():
     pytest.AMT = 1000
 
 
 @when('the acc holder withdraws 300')
 def withdraw_balance():
-    pytest.AMT = pytest.AMT - 700
+    pytest.AMT = pytest.AMT - 300
 
 @then('balans should to be 700')
 def final_balance():
