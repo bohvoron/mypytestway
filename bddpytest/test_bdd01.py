3a4e7da818be8b2a0fcb9a90e2bc7f8f7076fa75
bohvoron <160781963+bohvoron@users.noreply.github.com>
2024-08-26 17:44:35 +0200

    check some file in repo

---
+++ bddpytest/test_bdd01.py
@@ -0,0 +1,30 @@
+from pytest_bdd import scenario, scenarios, given, when, then
+from pathlib import Path
+
+feature_file_directory = 'bddfeatures'
+featureFile = 'number01.feature'
+BASE_DIR = Path(__file__).resolve().parent
+FEATURE_FILE = BASE_DIR.joinpath(feature_file_directory).joinpath(featureFile)
+
+
+def pytest_configure():
+    pytest.AMT = 0
+
+@scenario(FEATURE_FILE, 'Withdrawal of money')
+def test_withdrawal():
+    print('End of test Withdrawal')
+    pass
+
+@given('The balance is 1000 ')
+def current_balance():
+    pytest.AMT = 1000
+
+
+@when('the acc holder withdraws 300')
+def withdraw_balance():
+    pytest.AMT = pytest.AMT - 700
+
+@then('balans should to be 700')
+def final_balance():
+    assert pytest.AMT == 700
+
